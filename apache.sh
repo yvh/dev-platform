e739e2bb44cdedac935952ae176d2492dddbaf04
Yannick Vanhaeren <yannick.vanhaeren@gmail.com>
2018-05-02 11:07:44 +0200

    Add apache.sh

---
+++ apache.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -ex
+
+sudo apt update
+sudo apt install -y apache2
+
+sudo a2dismod mpm_event
+sudo a2enmod mpm_prefork rewrite
+echo "ServerName localhost" | sudo tee /etc/apache2/conf-available/fqdn.conf
+sudo a2enconf fqdn
+
+sudo sed -i 's#APACHE_RUN_USER=www-data#APACHE_RUN_USER=yannick#g' /etc/apache2/envvars
+sudo sed -i 's#APACHE_RUN_GROUP=www-data#APACHE_RUN_GROUP=yannick#g' /etc/apache2/envvars
+
+sudo systemctl restart apache2

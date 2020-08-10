8354c82c74511e78f74a5e5ed1b53c40b2e9e870
Yannick Vanhaeren <yvh@users.noreply.github.com>
2020-08-10 15:03:59 +0200

    Add apache2

--- build-env.sh
+++ build-env.sh
@@ -34,3 +34,16 @@ sudo apt update && sudo apt install -y atom
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm install -g yarn
+
+# apache
+sudo apt install -y apache2
+
+sudo a2dismod mpm_event
+sudo a2enmod mpm_prefork rewrite
+echo "ServerName localhost" | sudo tee /etc/apache2/conf-available/fqdn.conf
+sudo a2enconf fqdn
+
+sudo sed -i 's#APACHE_RUN_USER=www-data#APACHE_RUN_USER=yvh#g' /etc/apache2/envvars
+sudo sed -i 's#APACHE_RUN_GROUP=www-data#APACHE_RUN_GROUP=yvh#g' /etc/apache2/envvars
+
+sudo systemctl restart apache2

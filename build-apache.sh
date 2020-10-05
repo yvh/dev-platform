fb64781ae1d9eac2d1e532dce52e3543be666fb9
Yannick Vanhaeren <yvh@users.noreply.github.com>
2020-10-05 17:54:18 +0200

    Update build-apache.sh

--- build-apache.sh
+++ build-apache.sh
@@ -10,7 +10,7 @@ sudo a2enmod mpm_prefork rewrite
 echo "ServerName localhost" | sudo tee /etc/apache2/conf-available/fqdn.conf
 sudo a2enconf fqdn
 
-sudo sed -i 's#APACHE_RUN_USER=www-data#APACHE_RUN_USER=yannick#g' /etc/apache2/envvars
-sudo sed -i 's#APACHE_RUN_GROUP=www-data#APACHE_RUN_GROUP=yannick#g' /etc/apache2/envvars
+sudo sed -i 's#APACHE_RUN_USER=www-data#APACHE_RUN_USER=yvh#g' /etc/apache2/envvars
+sudo sed -i 's#APACHE_RUN_GROUP=www-data#APACHE_RUN_GROUP=yvh#g' /etc/apache2/envvars
 
 sudo systemctl restart apache2

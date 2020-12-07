4b1b019145ab1561422fa7b7215da070b094b901
Yannick Vanhaeren <yvh@users.noreply.github.com>
2020-12-07 16:25:33 +0100

    Update build-mariadb.sh

--- build-mariadb.sh
+++ build-mariadb.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
-sudo sh -c 'echo "deb [arch=amd64] https://archive.mariadb.org/repo/10.5/ubuntu groovy main" > /etc/apt/sources.list.d/mariadb.list'
-sudo sh -c 'echo "deb-src https://archive.mariadb.org/repo/10.5/ubuntu groovy main" >> /etc/apt/sources.list.d/mariadb.list'
+sudo apt-key --keyring /etc/apt/trusted.gpg.d/mariadb.gpg adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
+sudo sh -c 'echo "deb [arch=amd64] http://ams2.mirrors.digitalocean.com/mariadb/repo/10.5/ubuntu groovy main" > /etc/apt/sources.list.d/mariadb.list'
+sudo sh -c 'echo "#deb-src http://ams2.mirrors.digitalocean.com/mariadb/repo/10.5/ubuntu groovy main" >> /etc/apt/sources.list.d/mariadb.list'
 sudo apt update && sudo apt install -y mariadb-server mariadb-client
 sudo mysql_secure_installation

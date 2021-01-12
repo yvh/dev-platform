a8686ce012e15ae5d52bb748f13afaeed5751467
Yannick Vanhaeren <yvh@users.noreply.github.com>
2021-01-12 13:31:55 +0100

    Update build-env.sh

--- build-env.sh
+++ build-env.sh
@@ -6,36 +6,43 @@ set -ex
 sudo cp -v /usr/share/systemd/tmp.mount /etc/systemd/system/
 sudo systemctl enable --now tmp.mount
 
-# Upgrade & install some apps
+# upgrade & install some apps
 sudo add-apt-repository -y ppa:libreoffice/ppa
 sudo add-apt-repository -y ppa:git-core/ppa
 
 sudo apt -y full-upgrade
 sudo apt install -y build-essential apt-transport-https ca-certificates gnupg-agent software-properties-common \
-    tlp vim curl ubuntu-restricted-extras subversion git sshfs htop zsh gimp gimp-data-extras libreoffice libreoffice-style-breeze \
-    gnome-tweak-tool msmtp
+    tlp vim curl subversion git sshfs htop zsh gimp gimp-data-extras libreoffice libreoffice-style-breeze \
+    msmtp ttf-mscorefonts-installer
 sudo apt install -y --no-install-recommends kdiff3 wireshark
 
+# google chrome
 curl -SL https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o /tmp/google-chrome-stable_current_amd64.deb
 sudo apt install -y /tmp/google-chrome-stable_current_amd64.deb
 
+# atom
+#curl -sL https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key --keyring /etc/apt/trusted.gpg.d/atom.gpg add -
+#sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+#sudo apt update && sudo apt install -y --no-install-recommends atom
+
+# customization
 sudo update-alternatives --set editor /usr/bin/vim.basic
 sudo sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/' /root/.bashrc
 sudo sed -i 's/01;32m/01;31m/' /root/.bashrc
+chmod g-w msmtprc
 sudo cp msmtprc /etc/msmtprc
 
-sudo apt-get purge -y fonts-lohit* fonts-tlwg* fonts-samyak* fonts-tibetan-machine fonts-lklug-sinhala nano
+# remove uneccessary apps
+sudo apt-get purge -y fonts-lohit* fonts-tlwg* fonts-samyak* fonts-tibetan-machine fonts-lklug-sinhala nano firefox firefox-locale-en
+sudo apt autoremove --purge -y
+rm -r ~/.cache/mozilla ~/.mozilla
 
 # set mailhog
 sudo curl -SL https://github.com/mailhog/MailHog/releases/download/v1.0.1/MailHog_linux_amd64 -o /usr/local/bin/mailhog
 sudo chmod +x /usr/local/bin/mailhog
+chmod g-w mailhog.service
 sudo cp mailhog.service /etc/systemd/system/mailhog.service
 sudo systemctl enable --now mailhog
-
-# atom
-curl -sL https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key --keyring /etc/apt/trusted.gpg.d/atom.gpg add -
-sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
-sudo apt update && sudo apt install -y --no-install-recommends atom
 
 # nodejs & yarn
 #curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
@@ -74,6 +81,9 @@ sudo apt update && sudo apt install -y spotify-client
 # change inotify for idea (phpstorm)
 sudo sh -c 'echo "fs.inotify.max_user_watches = 524288" > /etc/sysctl.d/10-idea.conf'
 sudo sysctl -p --system
+
+# full-upgrade
+sudo apt -y full-upgrade
 
 # Oh my zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"

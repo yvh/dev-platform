326b56d8c6782158441e17d3934d8a6762939a16
Yannick Vanhaeren <yannick.vanhaeren@gmail.com>
2021-04-21 13:39:22 +0200

    Improve php build

--- build-php.sh
+++ build-php.sh
@@ -6,16 +6,16 @@ for VERSION in 7.3 7.4 8.0; do
     sudo apt install -y php${VERSION} php${VERSION}-{bz2,cli,curl,dev,gd,intl,ldap,mbstring,mysql,opcache,readline,xml,xsl,zip}
 
     for SYSTEM in apache2 cli; do
-        cd /etc/php/${VERSION}/${SYSTEM}
-        sudo sed -i -e "s|max_execution_time = 30|max_execution_time = 60|" php.ini
-        sudo sed -i -e "s|memory_limit = 128M|memory_limit = 512M|" php.ini
-        sudo sed -i -e "s|post_max_size = 8M|post_max_size = 55M|" php.ini
-        sudo sed -i -e "s|upload_max_filesize = 2M|upload_max_filesize = 50M|" php.ini
-        sudo sed -i -e "s|;date.timezone =|date.timezone = Europe/Brussels|" php.ini
-        sudo sed -i -e "s|;cgi.fix_pathinfo=1|cgi.fix_pathinfo = 0|" php.ini
-        sudo sed -i -e "s|;realpath_cache_ttl = 120|realpath_cache_ttl = 600|" php.ini
-        sudo sed -i -e "s|;realpath_cache_size = 4096k|realpath_cache_size = 4096K|" php.ini
-        sudo sed -i -e "s|;sendmail_path =|sendmail_path = /usr/bin/msmtp -t|" php.ini
+        ini_path=/etc/php/${VERSION}/${SYSTEM}
+        sudo sed -i -e "s|max_execution_time = 30|max_execution_time = 60|" $ini_path/php.ini
+        sudo sed -i -e "s|memory_limit = 128M|memory_limit = 512M|" $ini_path/php.ini
+        sudo sed -i -e "s|post_max_size = 8M|post_max_size = 55M|" $ini_path/php.ini
+        sudo sed -i -e "s|upload_max_filesize = 2M|upload_max_filesize = 50M|" $ini_path/php.ini
+        sudo sed -i -e "s|;date.timezone =|date.timezone = Europe/Brussels|" $ini_path/php.ini
+        sudo sed -i -e "s|;cgi.fix_pathinfo=1|cgi.fix_pathinfo = 0|" $ini_path/php.ini
+        sudo sed -i -e "s|;realpath_cache_ttl = 120|realpath_cache_ttl = 600|" $ini_path/php.ini
+        sudo sed -i -e "s|;realpath_cache_size = 4096k|realpath_cache_size = 4096K|" $ini_path/php.ini
+        sudo sed -i -e "s|;sendmail_path =|sendmail_path = /usr/bin/msmtp -t|" $ini_path/php.ini
     done
 done
 
@@ -31,32 +31,32 @@ for VERSION in 7.3 7.4 8.0; do
 
     sudo pecl -d php_suffix=${VERSION} install apcu ast ssh2-1.3.1 xdebug && sudo pecl uninstall -r apcu ast ssh2 xdebug
 
-    cd /etc/php/${VERSION}/mods-available
-    sudo sh -c 'echo "extension=apcu.so" > apcu.ini'
-    sudo sh -c 'echo "apc.enabled=1" >> apcu.ini'
-    sudo sh -c 'echo "apc.shm_segments=1" >> apcu.ini'
-    sudo sh -c 'echo "apc.shm_size=128M" >> apcu.ini'
-    sudo sh -c 'echo "apc.ttl=7200" >> apcu.ini'
-    sudo sh -c 'echo "apc.enable_cli=0" >> apcu.ini'
+    ini_path=/etc/php/${VERSION}/mods-available
+    sudo sh -c "echo \"extension=apcu.so\" > $ini_path/apcu.ini"
+    sudo sh -c "echo \"apc.enabled=1\" >> $ini_path/apcu.ini"
+    sudo sh -c "echo \"apc.shm_segments=1\" >> $ini_path/apcu.ini"
+    sudo sh -c "echo \"apc.shm_size=128M\" >> $ini_path/apcu.ini"
+    sudo sh -c "echo \"apc.ttl=7200\" >> $ini_path/apcu.ini"
+    sudo sh -c "echo \"apc.enable_cli=0\" >> $ini_path/apcu.ini"
 
-    sudo sh -c 'echo "extension=ast.so" > ast.ini'
+    sudo sh -c "echo \"extension=ast.so\" > $ini_path/ast.ini"
 
-    sudo sh -c 'echo "extension=ssh2.so" > ssh2.ini'
+    sudo sh -c "echo \"extension=ssh2.so\" > $ini_path/ssh2.ini"
 
-    sudo sh -c 'echo "opcache.enable=1" >> opcache.ini'
-    sudo sh -c 'echo "opcache.enable_cli=1" >> opcache.ini'
-    sudo sh -c 'echo "opcache.revalidate_freq=2" >> opcache.ini'
-    sudo sh -c 'echo "opcache.validate_timestamps=1" >> opcache.ini'
-    sudo sh -c 'echo "opcache.interned_strings_buffer=16" >> opcache.ini'
-    sudo sh -c 'echo "opcache.max_accelerated_files = 20000" >> opcache.ini'
+    sudo sh -c "echo \"opcache.enable=1\" >> $ini_path/opcache.ini"
+    sudo sh -c "echo \"opcache.enable_cli=1\" >> $ini_path/opcache.ini"
+    sudo sh -c "echo \"opcache.revalidate_freq=2\" >> $ini_path/opcache.ini"
+    sudo sh -c "echo \"opcache.validate_timestamps=1\" >> $ini_path/opcache.ini"
+    sudo sh -c "echo \"opcache.interned_strings_buffer=16\" >> $ini_path/opcache.ini"
+    sudo sh -c "echo \"opcache.max_accelerated_files = 20000\" >> $ini_path/opcache.ini"
 
-    sudo sh -c 'echo ";zend_extension=xdebug.so" > xdebug.ini'
-    sudo sh -c 'echo "xdebug.default_enable=1" >> xdebug.ini'
-    sudo sh -c 'echo "xdebug.profiler_enable=0" >> xdebug.ini'
-    sudo sh -c 'echo "xdebug.idekey=PHPSTORM" >> xdebug.ini'
-    sudo sh -c 'echo "xdebug.max_nesting_level=200" >> xdebug.ini'
-    sudo sh -c 'echo "xdebug.remote_enable=1" >> xdebug.ini'
-    sudo sh -c 'echo "xdebug.remote_autostart=0" >> xdebug.ini'
+    sudo sh -c "echo \";zend_extension=xdebug.so\" > $ini_path/xdebug.ini"
+    sudo sh -c "echo \"xdebug.default_enable=1\" >> $ini_path/xdebug.ini"
+    sudo sh -c "echo \"xdebug.profiler_enable=0\" >> $ini_path/xdebug.ini"
+    sudo sh -c "echo \"xdebug.idekey=PHPSTORM\" >> $ini_path/xdebug.ini"
+    sudo sh -c "echo \"xdebug.max_nesting_level=200\" >> $ini_path/xdebug.ini"
+    sudo sh -c "echo \"xdebug.remote_enable=1\" >> $ini_path/xdebug.ini"
+    sudo sh -c "echo \"xdebug.remote_autostart=0\" >> $ini_path/xdebug.ini"
 
     sudo phpenmod -v ${VERSION} apcu ast ssh2 xdebug
 done

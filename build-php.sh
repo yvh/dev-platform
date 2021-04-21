4c718a04566221e3d86bd01f81a14a52dad3cbdc
Yannick Vanhaeren <yannick.vanhaeren@gmail.com>
2021-04-21 11:03:07 +0200

    Add apache rules

--- build-php.sh
+++ build-php.sh
@@ -3,21 +3,20 @@
 set -ex
 
 for VERSION in 7.3 7.4 8.0; do
-  sudo apt install -y php${VERSION}-{bz2,cli,curl,dev,gd,intl,ldap,mbstring,mysql,opcache,readline,xml,xsl,zip}
+  sudo apt install -y php${VERSION} php${VERSION}-{bz2,cli,curl,dev,gd,intl,ldap,mbstring,mysql,opcache,readline,xml,xsl,zip}
 
-  if [[ ${VERSION} != 8\.* ]]; then
-      sudo apt install -y php${VERSION}-json
-  fi
-
-  cd /etc/php/${VERSION}/cli
-  sudo sed -i -e "s|max_execution_time = 30|max_execution_time = 60|" php.ini
-  sudo sed -i -e "s|memory_limit = 128M|memory_limit = 512M|" php.ini
-  sudo sed -i -e "s|post_max_size = 8M|post_max_size = 55M|" php.ini
-  sudo sed -i -e "s|upload_max_filesize = 2M|upload_max_filesize = 50M|" php.ini
-  sudo sed -i -e "s|;date.timezone =|date.timezone = Europe/Brussels|" php.ini
-  sudo sed -i -e "s|;cgi.fix_pathinfo=1|cgi.fix_pathinfo = 0|" php.ini
-  sudo sed -i -e "s|;realpath_cache_ttl = 120|realpath_cache_ttl = 600|" php.ini
-  sudo sed -i -e "s|;realpath_cache_size = 4096k|realpath_cache_size = 4096K|" php.ini
+  for SYSTEM in apache2 cli; do
+    cd /etc/php/${VERSION}/${SYSTEM}
+    sudo sed -i -e "s|max_execution_time = 30|max_execution_time = 60|" php.ini
+    sudo sed -i -e "s|memory_limit = 128M|memory_limit = 512M|" php.ini
+    sudo sed -i -e "s|post_max_size = 8M|post_max_size = 55M|" php.ini
+    sudo sed -i -e "s|upload_max_filesize = 2M|upload_max_filesize = 50M|" php.ini
+    sudo sed -i -e "s|;date.timezone =|date.timezone = Europe/Brussels|" php.ini
+    sudo sed -i -e "s|;cgi.fix_pathinfo=1|cgi.fix_pathinfo = 0|" php.ini
+    sudo sed -i -e "s|;realpath_cache_ttl = 120|realpath_cache_ttl = 600|" php.ini
+    sudo sed -i -e "s|;realpath_cache_size = 4096k|realpath_cache_size = 4096K|" php.ini
+    sudo sed -i -e "s|;sendmail_path =|sendmail_path = /usr/bin/msmtp -t|" php.ini
+  done
 done
 
 sudo apt install -y php-pear libssh2-1-dev
@@ -37,13 +36,10 @@ for VERSION in 7.3 7.4 8.0; do
   sudo sh -c 'echo "apc.shm_size=128M" >> apcu.ini'
   sudo sh -c 'echo "apc.ttl=7200" >> apcu.ini'
   sudo sh -c 'echo "apc.enable_cli=0" >> apcu.ini'
-  sudo phpenmod -v ${VERSION} -s cli apcu
   
   sudo sh -c 'echo "extension=ast.so" > ast.ini'
-  sudo phpenmod -v ${VERSION} -s cli ast
   
   sudo sh -c 'echo "extension=ssh2.so" > ssh2.ini'
-  sudo phpenmod -v ${VERSION} -s cli ssh2
 
   sudo sh -c 'echo "opcache.enable=1" >> opcache.ini'
   sudo sh -c 'echo "opcache.enable_cli=1" >> opcache.ini'
@@ -59,5 +55,8 @@ for VERSION in 7.3 7.4 8.0; do
   sudo sh -c 'echo "xdebug.max_nesting_level=200" >> xdebug.ini'
   sudo sh -c 'echo "xdebug.remote_enable=1" >> xdebug.ini'
   sudo sh -c 'echo "xdebug.remote_autostart=0" >> xdebug.ini'
-  sudo phpenmod -v ${VERSION} -s cli xdebug 
+
+  for SYSTEM in apache2 cli; do
+    sudo phpenmod -v ${VERSION} -s ${SYSTEM} apcu ast ssh2 xdebug
+  done
 done

970b36a93017c8d14ce295336eba98b19b25379a
Yannick Vanhaeren <yannick.vanhaeren@gmail.com>
2022-03-02 15:21:07 +0100

    Update mariadb repo

--- build-mariadb.sh
+++ build-mariadb.sh
@@ -3,6 +3,6 @@
 set -ex
 
 sudo apt-key --keyring /etc/apt/trusted.gpg.d/mariadb.gpg adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
-sudo sh -c 'echo "deb [arch=amd64] https://mirror.serverion.com/mariadb/repo/10.6/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/mariadb.list'
-sudo sh -c 'echo "#deb-src https://mirror.serverion.com/mariadb/repo/10.6/ubuntu $(lsb_release -cs) main" >> /etc/apt/sources.list.d/mariadb.list'
-sudo apt update && sudo apt install -y mariadb-server mariadb-client
+sudo sh -c 'echo "deb [arch=amd64] https://mirrors.xtom.nl/mariadb/repo/10.6/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/mariadb.list'
+sudo sh -c 'echo "#deb-src https://mirrors.xtom.nl/mariadb/repo/10.6/ubuntu $(lsb_release -cs) main" >> /etc/apt/sources.list.d/mariadb.list'
+sudo apt update && sudo apt install -y mariadb-server mariadb-client mariadb-backup

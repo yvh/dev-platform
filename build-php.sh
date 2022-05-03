8341881be3cf8d017e0650f135a013fa407a5f0d
Yannick Vanhaeren <yannick.vanhaeren@gmail.com>
2022-05-03 12:11:30 +0200

    Keep only php-cli

--- build-php.sh
+++ build-php.sh
@@ -3,20 +3,18 @@
 set -ex
 
 for VERSION in 7.4 8.0 8.1; do
-    sudo apt install -y php${VERSION} php${VERSION}-{bz2,cli,curl,dev,gd,intl,ldap,mbstring,mysql,opcache,soap,readline,xml,xsl,zip}
+    sudo apt install php${VERSION}-{bz2,cli,curl,dev,gd,intl,ldap,mbstring,mysql,opcache,soap,readline,xml,xsl,zip}
 
-    for SYSTEM in apache2 cli; do
-        ini_path=/etc/php/${VERSION}/${SYSTEM}
-        sudo sed -i -e "s|max_execution_time = 30|max_execution_time = 60|" $ini_path/php.ini
-        sudo sed -i -e "s|memory_limit = 128M|memory_limit = 512M|" $ini_path/php.ini
-        sudo sed -i -e "s|post_max_size = 8M|post_max_size = 55M|" $ini_path/php.ini
-        sudo sed -i -e "s|upload_max_filesize = 2M|upload_max_filesize = 50M|" $ini_path/php.ini
-        sudo sed -i -e "s|;date.timezone =|date.timezone = Europe/Brussels|" $ini_path/php.ini
-        sudo sed -i -e "s|;cgi.fix_pathinfo=1|cgi.fix_pathinfo = 0|" $ini_path/php.ini
-        sudo sed -i -e "s|;realpath_cache_ttl = 120|realpath_cache_ttl = 600|" $ini_path/php.ini
-        sudo sed -i -e "s|;realpath_cache_size = 4096k|realpath_cache_size = 4096K|" $ini_path/php.ini
-        sudo sed -i -e "s|;sendmail_path =|sendmail_path = /usr/bin/msmtp -t|" $ini_path/php.ini
-    done
+    ini_path=/etc/php/${VERSION}/cli
+    sudo sed -i -e "s|max_execution_time = 30|max_execution_time = 60|" $ini_path/php.ini
+    sudo sed -i -e "s|memory_limit = 128M|memory_limit = 512M|" $ini_path/php.ini
+    sudo sed -i -e "s|post_max_size = 8M|post_max_size = 55M|" $ini_path/php.ini
+    sudo sed -i -e "s|upload_max_filesize = 2M|upload_max_filesize = 50M|" $ini_path/php.ini
+    sudo sed -i -e "s|;date.timezone =|date.timezone = Europe/Brussels|" $ini_path/php.ini
+    sudo sed -i -e "s|;cgi.fix_pathinfo=1|cgi.fix_pathinfo = 0|" $ini_path/php.ini
+    sudo sed -i -e "s|;realpath_cache_ttl = 120|realpath_cache_ttl = 600|" $ini_path/php.ini
+    sudo sed -i -e "s|;realpath_cache_size = 4096k|realpath_cache_size = 4096K|" $ini_path/php.ini
+    sudo sed -i -e "s|;sendmail_path =|sendmail_path = /usr/bin/msmtp -t|" $ini_path/php.ini
 done
 
 sudo apt install -y php-pear libssh2-1-dev

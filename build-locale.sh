6145845624eaa95bc4f9b07ae8e5ade96f3496f8
Yannick Vanhaeren <yannick.vanhaeren@gmail.com>
2022-05-03 12:10:38 +0200

    Update build-locale

--- build-locale.sh
+++ build-locale.sh
@@ -7,6 +7,7 @@ sudo cp /tmp/en_BE /usr/share/i18n/locales/en_BE
 sudo localedef -i en_BE -c -f UTF-8 en_BE
 
 sudo sed -i -e "s|^de_BE\.UTF-8|# de_BE.UTF-8|" /etc/locale.gen
+sudo sed -i -e "s|^nl_BE\.UTF-8|# nl_BE.UTF-8|" /etc/locale.gen
 sudo sed -i -e "s|^# en_GB\.UTF-8|en_GB.UTF-8|" /etc/locale.gen
 sudo sed -i -e "s|^# fr_BE\.UTF-8|fr_BE.UTF-8|" /etc/locale.gen
 sudo sed -i -e "/# en_BW\.UTF-8.*/a en_BE.UTF-8 UTF-8" /etc/locale.gen
@@ -14,5 +15,5 @@ sudo sed -i -e "/# en_BW\.UTF-8.*/a en_BE.UTF-8 UTF-8" /etc/locale.gen
 sudo rm -f /var/lib/locales/supported.d/*
 sudo locale-gen
 
-sudo sed -i -e "s|en_GB|en_BE|g" /etc/default/locale
+sudo sed -i -e "s|nl_BE|en_BE|g" /etc/default/locale
 sudo sed -i -e "/LC_MEASUREMENT=.*/a LC_MESSAGES=C.UTF-8" /etc/default/locale

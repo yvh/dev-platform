133c478648d38683612ca41079f2c1c060604398
Yannick Vanhaeren <yannick.vanhaeren@gmail.com>
2022-08-15 15:50:04 +0200

    Update docker compose

--- build-env.sh
+++ build-env.sh
@@ -110,7 +110,7 @@ sudo usermod -aG docker yvh
 #sudo chmod +x /usr/local/bin/docker-compose
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v2.6.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v2.9.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 
 # spotify

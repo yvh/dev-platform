549ebe3f627affc140ee73822ac11c9ce7b58033
Yannick Vanhaeren <yvh@users.noreply.github.com>
2022-10-25 14:43:23 +0200

    Update with latest version

--- build-env.sh
+++ build-env.sh
@@ -2,30 +2,15 @@
 
 set -ex
 
-# /tmp to tmpfs
-sudo cp -v /usr/share/systemd/tmp.mount /etc/systemd/system/
-sudo systemctl enable --now tmp.mount
-
-# upgrade & install some apps
-#sudo add-apt-repository -y ppa:libreoffice/ppa
-sudo apt-key --keyring /etc/apt/trusted.gpg.d/libreoffice.gpg adv --keyserver keyserver.ubuntu.com --recv-key 36E81C9267FD1383FCC4490983FBA1751378B444
-sudo sh -c 'echo "deb https://ppa.launchpadcontent.net/libreoffice/ppa/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/libreoffice.list'
-sudo sh -c 'echo "#deb-src https://ppa.launchpadcontent.net/libreoffice/ppa/ubuntu $(lsb_release -cs) main" >> /etc/apt/sources.list.d/libreoffice.list'
-
-#sudo add-apt-repository -y ppa:remmina-ppa-team/remmina-next
-sudo apt-key --keyring /etc/apt/trusted.gpg.d/remmina.gpg adv --keyserver keyserver.ubuntu.com --recv-key 04E38CE134B239B9F38F82EE8A993C2521C5F0BA
-sudo sh -c 'echo "deb https://ppa.launchpadcontent.net/remmina-ppa-team/remmina-next/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/remmina.list'
-sudo sh -c 'echo "#deb-src https://ppa.launchpadcontent.net/remmina-ppa-team/remmina-next/ubuntu $(lsb_release -cs) main" >> /etc/apt/sources.list.d/remmina.list'
-
-sudo apt -y full-upgrade
+sudo apt update && sudo apt -y full-upgrade
 sudo apt install -y build-essential apt-transport-https ca-certificates gnupg-agent software-properties-common \
-    vim curl subversion sshfs htop zsh gimp gimp-data-extras libreoffice libreoffice-style-breeze \
-    filezilla hunspell-fr hunspell-fr-modern network-manager-fortisslvpn inkscape remmina \
-    ttf-bitstream-vera fonts-dejavu fonts-hack fonts-lato fonts-open-sans fonts-roboto fonts-powerline
+    vim curl subversion sshfs htop zsh gimp gimp-data-extras libreoffice libreoffice-style-breeze filezilla inkscape remmina \
+    ttf-bitstream-vera fonts-dejavu fonts-hack fonts-lato fonts-open-sans fonts-roboto fonts-powerline vlc \
+    ttf-mscorefonts-installer hunspell-fr network-manager-fortisslvpn cntlm openssh-server
 sudo apt install -y --no-install-recommends kdiff3 wireshark
 
 # github cli
-sudo apt-key --keyring /etc/apt/trusted.gpg.d/github.gpg adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+sudo apt-key --keyring /etc/apt/trusted.gpg.d/github.gpg adv --keyserver keyserver.ubuntu.com --recv-key 23F3D4EA75716059
 sudo sh -c 'echo "deb https://cli.github.com/packages stable main" > /etc/apt/sources.list.d/github.list'
 sudo sh -c 'echo "#deb-src https://cli.github.com/packages stable main" >> /etc/apt/sources.list.d/github.list'
 sudo apt update && sudo apt install -y gh
@@ -58,7 +43,7 @@ sudo cp msmtprc /etc/msmtprc
 
 # remove uneccessary apps
 sudo apt-get purge -y fonts-lohit* fonts-tlwg* fonts-samyak* fonts-tibetan-machine fonts-lklug-sinhala nano \
-    firefox firefox-locale-en skanlite kio-audiocd
+    firefox firefox-locale-en skanlite kio-audiocd thunderbird muon kde-config-tablet usb-creator-kde snapd
 sudo apt autoremove --purge -y
 rm -rf ~/.cache/mozilla ~/.mozilla
 
@@ -76,6 +61,7 @@ sudo systemctl enable --now mailhog
 #sudo sh -c 'echo "#deb-src https://deb.nodesource.com/node_16.x $(lsb_release -cs) main" >> /etc/apt/sources.list.d/nodesource.list'
 #sudo apt update && sudo apt install -y nodejs
 #sudo npm install -g yarn
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 # apache
 ./build-apache.sh
@@ -83,10 +69,9 @@ sudo systemctl enable --now mailhog
 # mariadb
 ./build-mariadb.sh
 
-#sudo add-apt-repository -y ppa:serge-rider/dbeaver-ce
-sudo apt-key --keyring /etc/apt/trusted.gpg.d/dbeaver-ce.gpg adv --keyserver keyserver.ubuntu.com --recv-key 30ECE32520D438C21E16BF884A71B51882788FD2
-sudo sh -c 'echo "deb https://ppa.launchpadcontent.net/serge-rider/dbeaver-ce/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/dbeaver-ce.list'
-sudo sh -c 'echo "#deb-src https://ppa.launchpadcontent.net/serge-rider/dbeaver-ce/ubuntu $(lsb_release -cs) main" >> /etc/apt/sources.list.d/dbeaver-ce.list'
+# dbeaver
+curl -sL https://dbeaver.io/debs/dbeaver.gpg.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/dbeaver-ce.gpg add -
+sudo sh -c 'echo "deb https://dbeaver.io/debs/dbeaver-ce /" > /etc/apt/sources.list.d/dbeaver-ce.list'
 sudo apt update && sudo apt install -y dbeaver-ce
 
 # php
@@ -109,13 +94,13 @@ sudo usermod -aG docker yvh
 #sudo chmod +x /usr/local/bin/docker-compose
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v2.9.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 
 # spotify
-curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key --keyring /etc/apt/trusted.gpg.d/spotify.gpg add -
-sudo sh -c 'echo "deb http://repository.spotify.com stable non-free" > /etc/apt/sources.list.d/spotify.list'
-sudo apt update && sudo apt install -y spotify-client
+#curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key --keyring /etc/apt/trusted.gpg.d/spotify.gpg add -
+#sudo sh -c 'echo "deb http://repository.spotify.com stable non-free" > /etc/apt/sources.list.d/spotify.list'
+#sudo apt update && sudo apt install -y spotify-client
 
 # change inotify for idea (phpstorm)
 sudo sh -c 'echo "fs.inotify.max_user_watches = 524288" > /etc/sysctl.d/10-idea.conf'
@@ -127,11 +112,11 @@ sudo apt -y full-upgrade
 sudo apt autoremove --purge -y
 
 # remove uneccessary dirs
-echo "enabled=False" > ~/.config/user-dirs.conf
-sed -i 's/XDG_TEMPLATES_DIR/#XDG_TEMPLATES_DIR/' ~/.config/user-dirs.dirs
-sed -i 's/XDG_PUBLICSHARE_DIR/#XDG_PUBLICSHARE_DIR/' ~/.config/user-dirs.dirs
-sed -i 's/XDG_MUSIC_DIR/#XDG_MUSIC_DIR/' ~/.config/user-dirs.dirs
-sed -i 's/XDG_VIDEOS_DIR=/#XDG_VIDEOS_DIR=/' ~/.config/user-dirs.dirs
+#echo "enabled=False" > ~/.config/user-dirs.conf
+#sed -i 's/XDG_TEMPLATES_DIR/#XDG_TEMPLATES_DIR/' ~/.config/user-dirs.dirs
+#sed -i 's/XDG_PUBLICSHARE_DIR/#XDG_PUBLICSHARE_DIR/' ~/.config/user-dirs.dirs
+#sed -i 's/XDG_MUSIC_DIR/#XDG_MUSIC_DIR/' ~/.config/user-dirs.dirs
+#sed -i 's/XDG_VIDEOS_DIR=/#XDG_VIDEOS_DIR=/' ~/.config/user-dirs.dirs
 
 # Oh my zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
--- build-mariadb.sh
+++ build-mariadb.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-sudo apt-key --keyring /etc/apt/trusted.gpg.d/mariadb.gpg adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
-sudo sh -c 'echo "deb [arch=amd64] https://mirrors.xtom.nl/mariadb/repo/10.6/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/mariadb.list'
-sudo sh -c 'echo "#deb-src https://mirrors.xtom.nl/mariadb/repo/10.6/ubuntu $(lsb_release -cs) main" >> /etc/apt/sources.list.d/mariadb.list'
+#sudo apt-key --keyring /etc/apt/trusted.gpg.d/mariadb.gpg adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
+#sudo sh -c 'echo "deb [arch=amd64] https://mirrors.xtom.nl/mariadb/repo/10.6/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/mariadb.list'
+#sudo sh -c 'echo "#deb-src https://mirrors.xtom.nl/mariadb/repo/10.6/ubuntu $(lsb_release -cs) main" >> /etc/apt/sources.list.d/mariadb.list'
 sudo apt update && sudo apt install -y mariadb-server mariadb-client mariadb-backup

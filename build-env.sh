651621fad191adf5f909371ccd4a2146f6f81a99
Yannick Vanhaeren <yvh@users.noreply.github.com>
2023-03-14 12:16:50 +0100

    Update with last versions

--- build-env.sh
+++ build-env.sh
@@ -7,15 +7,22 @@ sudo cp -v /usr/share/systemd/tmp.mount /etc/systemd/system/
 sudo systemctl enable --now tmp.mount
 
 # upgrade & install some apps
-#sudo add-apt-repository -y ppa:libreoffice/ppa
-sudo apt-key --keyring /etc/apt/trusted.gpg.d/libreoffice.gpg adv --keyserver keyserver.ubuntu.com --recv-key 36E81C9267FD1383FCC4490983FBA1751378B444
-sudo sh -c 'echo "deb https://ppa.launchpadcontent.net/libreoffice/ppa/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/libreoffice.list'
-sudo sh -c 'echo "#deb-src https://ppa.launchpadcontent.net/libreoffice/ppa/ubuntu $(lsb_release -cs) main" >> /etc/apt/sources.list.d/libreoffice.list'
+curl -fsSL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x36e81c9267fd1383fcc4490983fba1751378b444" | sudo gpg --dearmor -o /etc/apt/keyrings/libreoffice.gpg
+echo "Types: deb
+Architectures: amd64
+Signed-By: /etc/apt/keyrings/libreoffice.gpg
+URIs: https://ppa.launchpadcontent.net/libreoffice/ppa/ubuntu
+Suites: $(lsb_release -cs)
+Components: main" | sudo tee /etc/apt/sources.list.d/libreoffice.sources
 
-#sudo add-apt-repository -y ppa:remmina-ppa-team/remmina-next
-sudo apt-key --keyring /etc/apt/trusted.gpg.d/remmina.gpg adv --keyserver keyserver.ubuntu.com --recv-key 04E38CE134B239B9F38F82EE8A993C2521C5F0BA
-sudo sh -c 'echo "deb https://ppa.launchpadcontent.net/remmina-ppa-team/remmina-next/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/remmina.list'
-sudo sh -c 'echo "#deb-src https://ppa.launchpadcontent.net/remmina-ppa-team/remmina-next/ubuntu $(lsb_release -cs) main" >> /etc/apt/sources.list.d/remmina.list'
+curl -fsSL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x04e38ce134b239b9f38f82ee8a993c2521c5f0ba" | sudo gpg --dearmor -o /etc/apt/keyrings/remmina.gpg
+echo "Types: deb
+Architectures: amd64
+Signed-By: /etc/apt/keyrings/remmina.gpg
+URIs: https://ppa.launchpadcontent.net/remmina-ppa-team/remmina-next/ubuntu
+Suites: $(lsb_release -cs)
+Components: main" | sudo tee /etc/apt/sources.list.d/remmina.sources
+
 
 sudo apt update && sudo apt -y full-upgrade
 sudo apt install -y build-essential apt-transport-https ca-certificates gnupg-agent software-properties-common \
@@ -25,9 +32,13 @@ sudo apt install -y build-essential apt-transport-https ca-certificates gnupg-ag
 sudo apt install -y --no-install-recommends kdiff3 wireshark
 
 # github cli
-sudo apt-key --keyring /etc/apt/trusted.gpg.d/github.gpg adv --keyserver keyserver.ubuntu.com --recv-key 23F3D4EA75716059
-sudo sh -c 'echo "deb https://cli.github.com/packages stable main" > /etc/apt/sources.list.d/github.list'
-sudo sh -c 'echo "#deb-src https://cli.github.com/packages stable main" >> /etc/apt/sources.list.d/github.list'
+curl -fsSL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2c6106201985b60e6c7ac87323f3d4ea75716059" | sudo gpg --dearmor -o /etc/apt/keyrings/github.gpg
+echo "Types: deb
+Architectures: amd64
+Signed-By: /etc/apt/keyrings/github.gpg
+URIs: https://cli.github.com/packages
+Suites: stable
+Components: main" | sudo tee /etc/apt/sources.list.d/github.sources
 sudo apt update && sudo apt install -y gh
 
 # google chrome
@@ -39,11 +50,11 @@ curl -SL "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x
 sudo apt install -y /tmp/code_amd64.deb
 
 # teams
-curl -SL "https://go.microsoft.com/fwlink/p/?LinkID=2112886&clcid=0x80c&culture=fr-be&country=BE" -o /tmp/teams_amd64.deb
-sudo apt install -y /tmp/teams_amd64.deb
+#curl -SL "https://go.microsoft.com/fwlink/p/?LinkID=2112886&clcid=0x80c&culture=fr-be&country=BE" -o /tmp/teams_amd64.deb
+#sudo apt install -y /tmp/teams_amd64.deb
 
 # pdfsam
-curl -SL "https://github.com/torakiki/pdfsam/releases/download/v4.3.4/pdfsam_4.3.4-1_amd64.deb" -o /tmp/pdfsam.deb
+curl -SL "https://github.com/torakiki/pdfsam/releases/download/v5.0.3/pdfsam_5.0.3-1_amd64.deb" -o /tmp/pdfsam.deb
 sudo apt install -y /tmp/pdfsam.deb
 
 # postman
@@ -61,8 +72,8 @@ sudo sed -i 's/01;32m/01;31m/' /root/.bashrc
 sudo apt install -y msmtp
 chmod g-w msmtprc
 sudo cp msmtprc /etc/msmtprc
-sudo sh -c 'echo "[General]
-Numlock=on" >> /etc/sddm.conf'
+#sudo sh -c 'echo "[General]
+#Numlock=on" >> /etc/sddm.conf'
 sudo sh -c 'echo "[connectivity]
 enabled=false" > /etc/NetworkManager/conf.d/20-connectivity.conf'
 
@@ -80,13 +91,7 @@ sudo cp mailhog.service /etc/systemd/system/mailhog.service
 sudo systemctl enable --now mailhog
 
 # nodejs & yarn
-#curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-#curl -sL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/nodesource.gpg add -
-#sudo sh -c 'echo "deb https://deb.nodesource.com/node_16.x $(lsb_release -cs) main" > /etc/apt/sources.list.d/nodesource.list'
-#sudo sh -c 'echo "#deb-src https://deb.nodesource.com/node_16.x $(lsb_release -cs) main" >> /etc/apt/sources.list.d/nodesource.list'
-#sudo apt update && sudo apt install -y nodejs
-#sudo npm install -g yarn
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh" | bash
 
 # apache
 ./build-apache.sh
@@ -95,15 +100,22 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 ./build-mariadb.sh
 
 # dbeaver
-curl -sL https://dbeaver.io/debs/dbeaver.gpg.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/dbeaver-ce.gpg add -
-sudo sh -c 'echo "deb https://dbeaver.io/debs/dbeaver-ce /" > /etc/apt/sources.list.d/dbeaver-ce.list'
+curl -fsSL "https://dbeaver.io/debs/dbeaver.gpg.key"  | sudo gpg --dearmor -o /etc/apt/keyrings/dbeaver.gpg
+echo "Types: deb
+Architectures: amd64
+Signed-By: /etc/apt/keyrings/dbeaver.gpg
+URIs: https://dbeaver.io/debs/dbeaver-ce
+Suites: /" | sudo tee /etc/apt/sources.list.d/dbeaver-ce.sources
 sudo apt update && sudo apt install -y dbeaver-ce
 
 # php
-#sudo add-apt-repository -y ppa:ondrej/php
-sudo apt-key --keyring /etc/apt/trusted.gpg.d/php.gpg adv --keyserver keyserver.ubuntu.com --recv-key 14AA40EC0831756756D7F66C4F4EA0AAE5267A6C
-sudo sh -c 'echo "deb https://ppa.launchpadcontent.net/ondrej/php/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/php.list'
-sudo sh -c 'echo "#deb-src https://ppa.launchpadcontent.net/ondrej/php/ubuntu $(lsb_release -cs) main" >> /etc/apt/sources.list.d/php.list'
+curl -fsSL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x14aa40ec0831756756d7f66c4f4ea0aae5267a6c" | sudo gpg --dearmor -o /etc/apt/keyrings/php.gpg
+echo "Types: deb
+Architectures: amd64
+Signed-By: /etc/apt/keyrings/php.gpg
+URIs: https://ppa.launchpadcontent.net/ondrej/php/ubuntu
+Suites: $(lsb_release -cs)
+Components: main" | sudo tee /etc/apt/sources.list.d/php.sources
 sudo apt update
 ./build-php.sh
 sudo mkdir -p /var/www/html/phpinfo
@@ -111,20 +123,20 @@ sudo sh -c 'echo "<?php phpinfo();" > /var/www/html/phpinfo/index.php'
 curl -SL https://raw.githubusercontent.com/composer/getcomposer.org/main/web/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 
 # docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key --keyring /etc/apt/trusted.gpg.d/docker.gpg add -
-sudo sh -c 'echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list'
+curl -fsSL "https://download.docker.com/linux/ubuntu/gpg" | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "Types: deb
+Architectures: amd64
+Signed-By: /etc/apt/keyrings/docker.gpg
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(lsb_release -cs)
+Components: stable" | sudo tee /etc/apt/sources.list.d/docker.sources
 sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker yvh
 
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v2.15.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+curl -fsSL "https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-linux-x86_64" -o $DOCKER_CONFIG/cli-plugins/docker-compose
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
-
-# spotify
-#curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key --keyring /etc/apt/trusted.gpg.d/spotify.gpg add -
-#sudo sh -c 'echo "deb http://repository.spotify.com stable non-free" > /etc/apt/sources.list.d/spotify.list'
-#sudo apt update && sudo apt install -y spotify-client
 
 # change inotify for idea (phpstorm)
 sudo sh -c 'echo "fs.inotify.max_user_watches = 524288" > /etc/sysctl.d/10-idea.conf'
@@ -134,13 +146,6 @@ sudo sysctl -p --system
 sudo apt -y full-upgrade
 
 sudo apt autoremove --purge -y
-
-# remove uneccessary dirs
-#echo "enabled=False" > ~/.config/user-dirs.conf
-#sed -i 's/XDG_TEMPLATES_DIR/#XDG_TEMPLATES_DIR/' ~/.config/user-dirs.dirs
-#sed -i 's/XDG_PUBLICSHARE_DIR/#XDG_PUBLICSHARE_DIR/' ~/.config/user-dirs.dirs
-#sed -i 's/XDG_MUSIC_DIR/#XDG_MUSIC_DIR/' ~/.config/user-dirs.dirs
-#sed -i 's/XDG_VIDEOS_DIR=/#XDG_VIDEOS_DIR=/' ~/.config/user-dirs.dirs
 
 # Oh my zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"

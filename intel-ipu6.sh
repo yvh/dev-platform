c605df0ea04370510dfcc1fcbf489e9174ce54c7
Yannick Vanhaeren <yvh@users.noreply.github.com>
2023-03-14 15:46:09 +0100

    Update ipu6

--- intel-ipu6.sh
+++ intel-ipu6.sh
@@ -2,7 +2,12 @@
 
 set -ex
 
-sudo apt-key --keyring /etc/apt/trusted.gpg.d/intel-ipu6.gpg adv --keyserver keyserver.ubuntu.com --recv-key 7B85BB3BB81D9DAFF9F06250B52B913A41086767
-sudo sh -c 'echo "deb https://ppa.launchpadcontent.net/oem-solutions-group/intel-ipu6/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/intel-ipu6.list'
-sudo sh -c 'echo "#deb-src https://ppa.launchpadcontent.net/oem-solutions-group/intel-ipu6/ubuntu $(lsb_release -cs) main" >> /etc/apt/sources.list.d/intel-ipu6.list'
-sudo apt update && sudo apt install libcamhal-ipu6ep0
+curl -fsSL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x7b85bb3bb81d9daff9f06250b52b913a41086767" | sudo gpg --dearmor -o /etc/apt/keyrings/intel-ipu6.gpg
+echo "Types: deb
+Architectures: amd64
+Signed-By: /etc/apt/keyrings/intel-ipu6.gpg
+URIs: https://ppa.launchpadcontent.net/oem-solutions-group/intel-ipu6/ubuntu
+Suites: $(lsb_release -cs)
+Components: main" | sudo tee /etc/apt/sources.list.d/intel-ipu6.sources
+
+sudo apt update && sudo apt install -y libcamhal-ipu6ep0

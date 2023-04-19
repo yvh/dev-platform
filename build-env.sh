5a5b5d16534e68a9852586ae66638ceb31f2aef1
Yannick Vanhaeren <yannick.vanhaeren@gmail.com>
2023-04-19 21:56:33 +0200

    Add OC

--- build-env.sh
+++ build-env.sh
@@ -85,6 +85,10 @@ chmod g-w mailhog.service
 sudo cp mailhog.service /etc/systemd/system/mailhog.service
 sudo systemctl enable --now mailhog
 
+# OC
+curl -fsSL "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz" -o /tmp/openshift-client-linux.tar.gz
+sudo tar --extract --directory /usr/local/bin --file /tmp/openshift-client-linux.tar.gz oc kubectl
+
 # nodejs & yarn
 curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh" | bash
 

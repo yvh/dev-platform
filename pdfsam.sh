9bf1f3598e50f0dbfda7f764ebbbd05a95fb8126
Yannick Vanhaeren <yannick.vanhaeren@gmail.com>
2023-11-07 14:27:10 +0100

    Update pdfsam version

--- pdfsam.sh
+++ pdfsam.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-curl --silent --show-error --location "https://github.com/torakiki/pdfsam/releases/download/v5.1.3/pdfsam_5.1.3-1_amd64.deb" --output /tmp/pdfsam.deb
+curl --silent --show-error --location "https://github.com/torakiki/pdfsam/releases/download/v5.2.0/pdfsam_5.2.0-1_amd64.deb" --output /tmp/pdfsam.deb
 sudo apt install --assume-yes /tmp/pdfsam.deb
 rm --recursive --force /tmp/pdfsam.deb

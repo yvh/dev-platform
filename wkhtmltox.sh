9bbb033233330c18f54237e26b768d43962a9679
Yannick Vanhaeren <yannick.vanhaeren@gmail.com>
2024-02-06 11:14:02 +0100

    Update wkhtmltopdf

--- wkhtmltox.sh
+++ wkhtmltox.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-curl --silent --show-error --location "https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.jammy_amd64.deb" --output /tmp/wkhtmltox_amd64.deb
+curl --silent --show-error --location "https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-3/wkhtmltox_0.12.6.1-3.jammy_amd64.deb" --output /tmp/wkhtmltox_amd64.deb
 sudo apt install --assume-yes /tmp/wkhtmltox_amd64.deb
 rm --recursive --force /tmp/wkhtmltox_amd64.deb

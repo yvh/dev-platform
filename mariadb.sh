902e4070f046309be43b652638da9a3a05925d8b
Yannick Vanhaeren <yannick.vanhaeren@gmail.com>
2024-02-09 10:50:15 +0100

    Update mariadb repo

--- mariadb.sh
+++ mariadb.sh
@@ -7,7 +7,8 @@ echo "X-Repolib-Name: MariaDB
 Types: deb
 Architectures: amd64
 Signed-By: /etc/apt/keyrings/mariadb.pgp
-URIs: https://mariadb.mirror.wearetriple.com/repo/10.11/ubuntu
+#URIs: https://mariadb.mirror.wearetriple.com/repo/10.11/ubuntu
+URIs: https://mirrors.xtom.nl/mariadb/repo/10.11/ubuntu
 Suites: $(lsb_release --codename --short)
 Components: main main/debug" | sudo tee /etc/apt/sources.list.d/mariadb.sources
 sudo apt update && sudo apt install --assume-yes mariadb-server mariadb-client mariadb-backup

3510db8f4bc3d5debc27c4527af24a4141d78650
Yannick Vanhaeren <yannick.vanhaeren@gmail.com>
2024-04-16 14:25:13 +0200

    Remove unnecessary files

--- nvidia.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-sudo ubuntu-drivers install
--- pdfsam.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-curl --silent --show-error --location "https://github.com/torakiki/pdfsam/releases/download/v5.2.2/pdfsam_5.2.2-1_amd64.deb" --output /tmp/pdfsam.deb
-sudo apt install --assume-yes /tmp/pdfsam.deb
-rm --recursive --force /tmp/pdfsam.deb

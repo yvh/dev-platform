6038fabe7c6751525abb2896ae192c9e53aee429
Yannick Vanhaeren <yannick.vanhaeren@gmail.com>
2024-04-16 14:25:00 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -6,33 +6,33 @@ set -ex
 sudo cp /usr/share/systemd/tmp.mount /etc/systemd/system/
 sudo systemctl enable --now tmp.mount
 
-# remove preinstalled-pool
-sudo rm --recursive --force /var/lib/preinstalled-pool
-sudo rm --force /etc/apt/sources.list.d/preinstalled-pool.list
-sudo rm --force /etc/apt/trusted.gpg
-
 # upgrade & install some apps
 sudo apt update && sudo apt full-upgrade --assume-yes
 sudo apt install --assume-yes build-essential apt-transport-https ca-certificates gnupg-agent software-properties-common \
-    vim curl sshfs htop zsh gimp gimp-data-extras filezilla inkscape git-flow \
-    ttf-bitstream-vera fonts-dejavu fonts-hack fonts-lato fonts-open-sans fonts-roboto fonts-powerline vlc \
-    ttf-mscorefonts-installer hunspell-fr cntlm openssh-server jq network-manager-fortisslvpn
+    vim curl sshfs htop zsh filezilla git-flow \
+    ttf-bitstream-vera fonts-dejavu fonts-hack fonts-lato fonts-open-sans fonts-roboto fonts-powerline gnome-tweaks \
+    aspell-fr hyphen-fr mythes-fr hunspell-fr ttf-mscorefonts-installer cntlm jq terminator imagemagick
 sudo apt install --assume-yes --no-install-recommends kdiff3 wireshark
+
+# remove snapd
+sudo snap remove firefox gnome-42-2204
+sudo snap remove gtk-common-themes
+sudo snap remove snapd-desktop-integration
+sudo snap remove snap-store
+sudo snap remove core22
+sudo snap remove bare
+sudo snap remove snapd
+sudo apt -y autoremove --purge snapd
+rm -rf ~/snap ~/Downloads/firefox.tmp
 
 # libreoffice
 ./libreoffice.sh
-
-# remmina
-./remmina.sh
 
 # google chrome
 ./google-chrome.sh
 
 # visual studio code
 ./visual-studio-code.sh
-
-# pdfsam
-./pdfsam.sh
 
 # postman
 ./postman.sh
@@ -48,15 +48,10 @@ sudo update-alternatives --set editor /usr/bin/vim.basic
 sudo sed --in-place 's/#force_color_prompt=yes/force_color_prompt=yes/' /root/.bashrc
 sudo sed --in-place 's/01;32m/01;31m/' /root/.bashrc
 sudo sed --in-place 's/    SendEnv/#   SendEnv/g' /etc/ssh/ssh_config
-#sudo sh -c 'echo "[General]
-#Numlock=on" >> /etc/sddm.conf'
-sudo sh -c 'echo "[connectivity]
-enabled=false" > /etc/NetworkManager/conf.d/20-connectivity.conf'
 
 # remove uneccessary apps
 sudo apt-get purge --assume-yes fonts-lohit* fonts-tlwg* fonts-samyak* fonts-tibetan-machine fonts-lklug-sinhala nano \
-    firefox firefox-locale-en skanlite kio-audiocd thunderbird muon kde-config-tablet usb-creator-kde snapd \
-    plasma-welcome partitionmanager
+    firefox firefox-locale-en skanlite kio-audiocd thunderbird
 sudo apt autoremove --purge --assume-yes
 rm --recursive --force ~/.cache/mozilla ~/.mozilla
 

db54f5cd0366796b32b1250a24bab350a035ec1c
Yannick Vanhaeren <yvh@users.noreply.github.com>
2024-04-16 14:24:41 +0200

    Update jetbrains install script

--- jetbrains-toolbox.sh
+++ jetbrains-toolbox.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
+sudo apt update
+sudo apt install --assume-yes libfuse2
+
 if [[ ! -f ~/.local/share/JetBrains/Toolbox/bin/jetbrains-toolbox ]]; then
     URL=$(curl --silent "https://data.services.jetbrains.com//products/releases?code=TBA&latest=true&type=release" | jq --raw-output ".TBA[0].downloads.linux.link")
     DOWNLOAD_TEMP_DIR=$(mktemp --directory)

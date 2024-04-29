c60ddfffa3dfecb8ffd45f84e2a438705365a977
Yannick Vanhaeren <yannick.vanhaeren@gmail.com>
2024-04-29 16:25:32 +0200

    Update pdfsam script to get latest release

--- pdfsam.sh
+++ pdfsam.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-curl --silent --show-error --location "https://github.com/torakiki/pdfsam/releases/download/v5.2.2/pdfsam_5.2.2-1_amd64.deb" --output /tmp/pdfsam.deb
+curl --silent --show-error --location $(curl --silent --location https://api.github.com/repos/torakiki/pdfsam/releases/latest | jq --raw-output '.assets[] | select(.name | match("amd64.deb$")) | .browser_download_url') --output /tmp/pdfsam.deb
 sudo apt install --assume-yes /tmp/pdfsam.deb
 rm --recursive --force /tmp/pdfsam.deb

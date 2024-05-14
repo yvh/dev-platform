dabfd3a9944fc02b75ad330a215a2c4e71e568a1
Yannick Vanhaeren <yannick.vanhaeren@gmail.com>
2024-05-14 10:54:25 +0200

    Typo change

--- falco.sh
+++ falco.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-curl --silent --show-error --location $(curl --silent --location https://api.github.com/repos/horussoftware/falco-fiscalteam3_4/releases/latest | jq --raw-output '.assets[] | select(.name | match("amd64.deb$")) | .browser_download_url') --output /tmp/falco.deb
+curl --silent --show-error --location "$(curl --silent --location https://api.github.com/repos/horussoftware/falco-fiscalteam3_4/releases/latest | jq --raw-output '.assets[] | select(.name | match("amd64.deb$")) | .browser_download_url')" --output /tmp/falco.deb
 sudo apt install --assume-yes /tmp/falco.deb
 rm --recursive --force /tmp/falco.deb

03a8eb65ad0539ebc003c6bc297d8e74fb8cc2d1
Yannick Vanhaeren <yannick.vanhaeren@gmail.com>
2024-05-14 09:18:10 +0200

    Update pdfsam

--- pdfsam.sh
+++ pdfsam.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-curl --silent --show-error --location $(curl --silent --location https://api.github.com/repos/torakiki/pdfsam/releases/latest | jq --raw-output '.assets[] | select(.name | match("amd64.deb$")) | .browser_download_url') --output /tmp/pdfsam.deb
+curl --silent --show-error --location "$(curl --silent --location https://api.github.com/repos/torakiki/pdfsam/releases/latest | jq --raw-output '.assets[] | select(.name | match("amd64.deb$")) | .browser_download_url')" --output /tmp/pdfsam.deb
 sudo apt install --assume-yes /tmp/pdfsam.deb
 rm --recursive --force /tmp/pdfsam.deb

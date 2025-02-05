1b309fb36d4fbe92c102832a3579f7915b09f9ce
Yannick Vanhaeren <yvh@users.noreply.github.com>
2025-02-05 16:41:02 +0100

    Add apache

--- build.sh
+++ build.sh
@@ -39,6 +39,9 @@ sudo apt-get purge --assume-yes fonts-lohit* fonts-tlwg* fonts-samyak* fonts-tib
 sudo apt autoremove --purge --assume-yes
 rm --recursive --force ~/.cache/mozilla ~/.mozilla
 
+# apache
+./apache.sh
+
 # docker
 ./docker.sh
 

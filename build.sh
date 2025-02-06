267b0dc5529fc4c05d98fabceb13420a36827fdf
Yannick Vanhaeren <yannick.vanhaeren@gmail.com>
2025-02-06 15:12:51 +0100

    Remove vlc

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ sudo systemctl enable --now tmp.mount
 # upgrade & install some apps
 sudo apt update && sudo apt full-upgrade --assume-yes
 sudo apt install --assume-yes build-essential apt-transport-https ca-certificates gnupg-agent software-properties-common \
-    vim curl sshfs htop zsh gimp gimp-data-extras filezilla inkscape cntlm jq terminator vlc \
+    vim curl sshfs htop zsh gimp gimp-data-extras filezilla inkscape cntlm jq terminator \
     ttf-bitstream-vera fonts-dejavu fonts-hack fonts-lato fonts-open-sans fonts-roboto fonts-powerline \
     aspell-fr hyphen-fr mythes-fr hunspell-fr network-manager-fortisslvpn-gnome imagemagick
 sudo apt install --assume-yes --no-install-recommends kdiff3 wireshark kompare

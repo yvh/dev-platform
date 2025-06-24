3d138cf8c4234f00d853368153487a75a8bc0a37
Yannick Vanhaeren <yvh@users.noreply.github.com>
2025-06-24 16:04:51 +0200

    Use verbose arguments

--- build.sh
+++ build.sh
@@ -83,4 +83,4 @@ sed -i 's/XDG_VIDEOS_DIR=/#XDG_VIDEOS_DIR=/' ~/.config/user-dirs.dirs
 
 # oh-my-zsh
 set +x
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl --silent --show-error --location https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"

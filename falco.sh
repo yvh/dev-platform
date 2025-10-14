10e12dec17b038101ed00df538a1a7a02ec1900c
Yannick Vanhaeren <yvh@users.noreply.github.com>
2025-10-14 19:13:05 +0200

    Apt alignment

--- falco.sh
+++ falco.sh
@@ -12,5 +12,5 @@ fi
 
 echo "💰 Installing Falco..."
 curl --silent --show-error --fail --location "$(curl --silent --location https://api.github.com/repos/horussoftware/falco-fiscalteam3_4/releases/latest | jq --raw-output '.assets[] | select(.name | match("amd64.deb$")) | .browser_download_url')" --output /tmp/falco.deb
-apt install --assume-yes /tmp/falco.deb
+apt install --no-install-recommends --no-install-suggests --assume-yes /tmp/falco.deb
 rm --recursive --force /tmp/falco.deb
--- glab.sh
+++ glab.sh
@@ -12,5 +12,5 @@ fi
 
 echo "🐙 Installing GitLab CLI (glab)..."
 curl --silent --show-error --fail --location "$(curl --silent --location https://gitlab.com/api/v4/projects/gitlab-org%2Fcli/releases | jq --raw-output '.[0].assets.links[] | select(.name | match("amd64.deb$")) | .direct_asset_url')" --output /tmp/glab.deb
-apt install --assume-yes /tmp/glab.deb
+apt install --no-install-recommends --no-install-suggests --assume-yes /tmp/glab.deb
 rm --recursive --force /tmp/glab.deb

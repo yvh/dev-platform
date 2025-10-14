8bc6c8dd9c82322b5c9675946fa712fc660b1b66
Yannick Vanhaeren <yvh@users.noreply.github.com>
2025-10-14 13:00:49 +0200

    Update mariadb.sh

--- mariadb.sh
+++ mariadb.sh
@@ -11,12 +11,13 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 echo "🐬 Installing MariaDB client..."
-#curl --silent --show-error --fail --location --output /etc/apt/keyrings/mariadb.pgp "https://mariadb.org/mariadb_release_signing_key.pgp"
-#echo "X-Repolib-Name: MariaDB
-#Types: deb
-#Architectures: amd64
-#Signed-By: /etc/apt/keyrings/mariadb.pgp
-#URIs: https://mirrors.xtom.de/mariadb/repo/11.4/debian
-#Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
-#Components: main" | tee /etc/apt/sources.list.d/mariadb.sources > /dev/null
+mkdir --parents /etc/apt/keyrings
+curl --silent --show-error --fail --location --output /etc/apt/keyrings/mariadb-keyring.pgp "https://mariadb.org/mariadb_release_signing_key.pgp"
+echo "X-Repolib-Name: MariaDB
+Types: deb
+Architectures: amd64
+Signed-By: /etc/apt/keyrings/mariadb-keyring.pgp
+URIs: https://deb.mariadb.org/11.8/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: main" | tee /etc/apt/sources.list.d/mariadb.sources > /dev/null
 apt update && apt install --no-install-recommends --no-install-suggests --assume-yes mariadb-client

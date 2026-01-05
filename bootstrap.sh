0648177a9329ce59670d933eb42d9e40c126b669
Yannick Vanhaeren <yvh@users.noreply.github.com>
2026-01-05 13:28:44 +0100

    Update bootstrap.sh for Debian repository configuration

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,14 +22,21 @@ APT::Install-Suggests "false";
 EOF
 
 echo ""
-echo "🔧 Enabling backports repository..."
-cat > /etc/apt/sources.list.d/debian-backports.sources << EOF
+echo "🔧 Enabling debian repository..."
+cat > /etc/apt/sources.list.d/debian.sources << EOF
 Types: deb
 Architectures: amd64
 Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
-URIs: http://deb.debian.org/debian
-Suites:  $(lsb_release --codename --short)-backports
-Components: main contrib non-free
+URIs: https://deb.debian.org/debian
+Suites: $(lsb_release --codename --short) $(lsb_release --codename --short)-updates $(lsb_release --codename --short)-backports
+Components: main contrib non-free non-free-firmware
+
+Types: deb
+Architectures: amd64
+Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+URIs: https://security.debian.org/debian-security
+Suites: $(lsb_release --codename --short)-security
+Components: main contrib non-free non-free-firmware
 EOF
 
 echo "📦 Setting high priority for backports..."
@@ -40,8 +47,8 @@ Pin-Priority: 900
 EOF
 
 echo ""
-echo "🧹 Cleaning up unnecessary deb-src entries..."
-sed --in-place --expression "s|^deb-src|#deb-src|" /etc/apt/sources.list
+echo "🧹 Cleaning up unnecessary sources.list..."
+rm --force /etc/apt/sources.list{,~}
 
 echo ""
 echo "🆙 Updating and upgrading the system..."

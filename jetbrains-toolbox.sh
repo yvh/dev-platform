880c87355da2b10fb7c89b243d7b2ba2540ae912
Yannick Vanhaeren <yvh@users.noreply.github.com>
2026-01-20 15:43:41 +0100

    Refactor JetBrains Toolbox installation script

--- jetbrains-toolbox.sh
+++ jetbrains-toolbox.sh
@@ -12,7 +12,9 @@ fi
 
 echo "⚡ Installing JetBrains Toolbox..."
 user=${USER_OVERRIDE:-$(getent passwd 1000 | cut -d: -f1)}
-jetbrains_directory="$(getent passwd ${user} | cut -d: -f6)/.local/share/JetBrains"
-mkdir --parents ${jetbrains_directory}/Toolbox
-curl --silent --show-error --location "$(curl --silent --location "https://data.services.jetbrains.com//products/releases?code=TBA&latest=true&type=release" | jq --raw-output ".TBA[0].downloads.linux.link")" | tar --extract --gzip --directory ${jetbrains_directory}/Toolbox --strip-components=1
-chown --recursive ${user}: ${jetbrains_directory}
+user_home="$(getent passwd ${user} | cut -d: -f6)"
+install --directory --owner ${user} --group ${user} "${user_home}/.local"
+install --directory --owner ${user} --group ${user} "${user_home}/.local/share"
+install --directory --owner ${user} --group ${user} "${user_home}/.local/share/JetBrains"
+install --directory --owner ${user} --group ${user} "${user_home}/.local/share/JetBrains/Toolbox"
+curl --silent --show-error --location "$(curl --silent --location "https://data.services.jetbrains.com//products/releases?code=TBA&latest=true&type=release" | jq --raw-output ".TBA[0].downloads.linux.link")" | runuser --user ${user} -- tar --extract --gzip --directory "${user_home}/.local/share/JetBrains/Toolbox" --strip-components=1

1ca104b98abdc62cf74c2a603bd02dcf8dba9c47
Yannick Vanhaeren <yvh@users.noreply.github.com>
2026-01-28 13:05:50 +0100

    Remove stunnel configuration writing from installation script

--- stunnel.sh
+++ stunnel.sh
@@ -14,15 +14,3 @@ echo "🔐 Installing stunnel4..."
 apt update && apt install --no-install-recommends --no-install-suggests --assume-yes stunnel4
 systemctl disable stunnel4
 systemctl stop stunnel4 || true
-
-echo "📄 Writing stunnel config..."
-mkdir --parents /etc/stunnel
-
-cat > /etc/stunnel/ssh-tls.conf << 'EOF'
-foreground = yes
-client = yes
-
-[ssh-tls]
-accept = 127.0.0.1:4430
-connect = 141.227.186.72:443
-EOF

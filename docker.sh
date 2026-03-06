a9cab97f3b89e25a57181f5559af2097ebf3b204
Yannick Vanhaeren <yvh@users.noreply.github.com>
2026-03-06 15:21:39 +0100

    Restore docker

---
+++ docker.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ "$EUID" -ne 0 ]; then
+  echo ""
+  echo "⚠️  ‘You are not root, young hobbit...’"
+  echo "👑 Elevating your privileges... like a true wizard."
+  echo ""
+  exec sudo --preserve-env bash "$0" "$@"
+fi
+
+echo "🐳 Installing Docker..."
+curl --silent --show-error --fail --location "https://download.docker.com/linux/debian/gpg" | gpg --dearmor --output /etc/apt/keyrings/docker.gpg > /dev/null
+cat > /etc/apt/sources.list.d/docker.sources << EOF
+Types: deb
+Architectures: amd64
+Signed-By: /etc/apt/keyrings/docker.gpg
+URIs: https://download.docker.com/linux/debian
+Suites: $(lsb_release --codename --short)
+Components: stable
+EOF
+apt update && apt install --no-install-recommends --no-install-suggests --assume-yes \
+  containerd.io \
+  docker-buildx-plugin \
+  docker-ce \
+  docker-ce-cli \
+  docker-compose-plugin
+
+mkdir --parents /etc/docker
+cat > /etc/docker/daemon.json <<'EOF'
+{
+    "features": {
+        "buildkit": true
+    },
+    "log-driver": "local",
+    "log-opts": {
+        "max-size": "20m",
+        "max-file": "5"
+    },
+    "max-concurrent-downloads": 10,
+    "max-concurrent-uploads": 5
+}
+EOF
+systemctl restart docker.service
+
+echo "👤 Adding a user to the docker group..."
+usermod --append --groups docker ${USER_OVERRIDE:-$(getent passwd 1000 | cut -d: -f1)}

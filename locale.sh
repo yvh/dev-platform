8cf796417b85b2c96e7b0adbf4c12f39b858262c
Yannick Vanhaeren <yvh@users.noreply.github.com>
2026-03-17 13:07:56 +0000

    Update locale

--- locale.sh
+++ locale.sh
@@ -14,23 +14,23 @@ echo "🌍 Setting system locale..."
 curl --silent --show-error --fail --location --output /usr/share/i18n/locales/en_BE "https://gist.githubusercontent.com/yvh/630368018d7c683aca8da9e2baf7bfb9/raw/48d0bf07c296fabb8d927317e2a1ac0a271c313b/en_BE"
 localedef --inputfile en_BE --charmap UTF-8 --force en_BE
 
-sed --in-place '$d' /etc/locale.gen
-sed --in-place --expression "s|^de_BE\.UTF-8|# de_BE.UTF-8|" /etc/locale.gen
-sed --in-place --expression "s|^nl_BE\.UTF-8|# nl_BE.UTF-8|" /etc/locale.gen
-sed --in-place --expression "s|^# en_GB\.UTF-8|en_GB.UTF-8|" /etc/locale.gen
-sed --in-place --expression "s|^# en_US\.UTF-8|en_US.UTF-8|" /etc/locale.gen
-sed --in-place --expression "s|^# fr_BE\.UTF-8|fr_BE.UTF-8|" /etc/locale.gen
-sed --in-place --expression "/# en_BW\.UTF-8.*/a en_BE.UTF-8 UTF-8" /etc/locale.gen
+sed --in-place --expression "s|^#en_US\.UTF-8|en_US.UTF-8|" /etc/locale.gen
+sed --in-place --expression "s|^#fr_BE\.UTF-8|fr_BE.UTF-8|" /etc/locale.gen
+sed --in-place --expression "/#en_BW\.UTF-8.*/a en_BE.UTF-8 UTF-8" /etc/locale.gen
 
-cat >> /etc/default/locale << EOF
-LC_ADDRESS="en_BE.UTF-8"    
-LC_MEASUREMENT="en_BE.UTF-8"
-LC_MONETARY="en_BE.UTF-8"
-LC_NUMERIC="en_BE.UTF-8"
-LC_PAPER="en_BE.UTF-8"
-LC_TELEPHONE="en_BE.UTF-8"
-LC_TIME="en_BE.UTF-8"
+cat > /etc/locale.conf << 'EOF'
+LANG=en_US.UTF-8
+LC_ADDRESS=en_BE.UTF-8
+LC_MEASUREMENT=en_BE.UTF-8
+LC_MONETARY=en_BE.UTF-8
+LC_NUMERIC=en_BE.UTF-8
+LC_PAPER=en_BE.UTF-8
+LC_TELEPHONE=en_BE.UTF-8
+LC_TIME=en_BE.UTF-8
+EOF
+
+cat > /etc/vconsole.conf << 'EOF'
+KEYMAP=be-latin1
 EOF
 
 locale-gen
-update-locale

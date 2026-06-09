4ef97c364696a90ed5d824525f86084525280473
Yannick Vanhaeren <yvh@users.noreply.github.com>
2026-06-09 10:45:53 +0200

    Modify locale.sh to set en_IE.UTF-8
    Update locale settings to use en_IE.UTF-8 and remove en_BE configurations.

--- locale.sh
+++ locale.sh
@@ -14,26 +14,8 @@ echo "🌍 Setting system locale..."
 ln --symbolic --force /usr/share/zoneinfo/Europe/Brussels /etc/localtime
 hwclock --systohc
 
-curl --silent --show-error --fail --location --output /usr/share/i18n/locales/en_BE "https://gist.githubusercontent.com/yvh/630368018d7c683aca8da9e2baf7bfb9/raw/48d0bf07c296fabb8d927317e2a1ac0a271c313b/en_BE"
-localedef --inputfile en_BE --charmap UTF-8 --force en_BE
-
-sed --in-place --expression "s|^#en_US\.UTF-8|en_US.UTF-8|" /etc/locale.gen
-sed --in-place --expression "s|^#fr_BE\.UTF-8|fr_BE.UTF-8|" /etc/locale.gen
-sed --in-place --expression "/#en_BW\.UTF-8.*/a en_BE.UTF-8 UTF-8" /etc/locale.gen
-
-cat > /etc/locale.conf << 'EOF'
-LANG=en_US.UTF-8
-LC_ADDRESS=en_BE.UTF-8
-LC_MEASUREMENT=en_BE.UTF-8
-LC_MONETARY=en_BE.UTF-8
-LC_NUMERIC=en_BE.UTF-8
-LC_PAPER=en_BE.UTF-8
-LC_TELEPHONE=en_BE.UTF-8
-LC_TIME=en_BE.UTF-8
-EOF
-
-cat > /etc/vconsole.conf << 'EOF'
-KEYMAP=be-latin1
-EOF
+sed --in-place 's/^#en_IE.UTF-8 UTF-8/en_IE.UTF-8 UTF-8/' /etc/locale.gen
+echo "LANG=en_IE.UTF-8" > /etc/locale.conf
+sed --in-place 's|LANG=${LANG:-C.UTF-8}|[ -f /etc/locale.conf ] \&\& . /etc/locale.conf; LANG=${LANG:-C.UTF-8}|' /etc/profile.d/locale.sh
 
 locale-gen
